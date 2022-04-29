a630f9238574cc796e1465dc394c8a7404c05e77
GiovanniGuarnieri <101807148+GiovanniGuarnieri@users.noreply.github.com>
2022-04-29 15:14:28 +0200

    Update NegozioSb1Application.java
    ciao

--- src/main/java/it/epicode/negozio/NegozioSb1Application.java
+++ src/main/java/it/epicode/negozio/NegozioSb1Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NegozioSb1Application {
-
+//prova
 	public static void main(String[] args) {
 		SpringApplication.run(NegozioSb1Application.class, args);
 	}
